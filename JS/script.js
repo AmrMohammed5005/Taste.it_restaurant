d2131645326c6b360bb8b2147331bd0873c3b7c6
Amr Mohammed <101597988+AmrMohammed5005@users.noreply.github.com>
2023-06-18 04:58:12 +0300

    Created Restaurant Website

---
+++ JS/script.js
@@ -0,0 +1,201 @@
+// Home Carousel
+$('.home-carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 7000,
+    animateOut: 'fadeOut',
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
+
+//Navbar
+window.addEventListener("scroll", function () {
+    let navbar = document.querySelector("#navbar");
+    // Toggles Fixed Class In Navbar On Scrolling
+    navbar.classList.toggle('fixed', this.window.scrollY > 0)
+});
+
+// Nav Buttons
+let menuBtn = document.querySelector('.menu-btn');
+let searchBtn = document.querySelector('.searchbtn');
+let cartBtn = document.querySelector('.cartbtn');
+let darkBtn = document.querySelector('.darkbtn');
+let signImg = document.querySelector('#signImg');
+
+// Menu btn
+menuBtn.onclick = function () {
+    // Toggles Active Class In Nav Items On Click
+    document.querySelector(".nav-items").classList.toggle("active");
+    // Changes Icons on Click
+    if (document.querySelector(".nav-items").classList.contains('active')) {
+        menuBtn.classList.remove("bx-menu");
+        menuBtn.classList.add("bx-x");
+    }
+    else {
+        menuBtn.classList.remove("bx-x");
+        menuBtn.classList.add("bx-menu");
+    }
+}
+
+searchBtn.onclick = function () {
+    // Toggles Active Class In Search Form On Click
+    document.querySelector("#search-form").classList.toggle("active");
+    // Changes Icons on Click
+    if (document.querySelector("#search-form").classList.contains('active')) {
+        searchBtn.classList.remove("bx-search-alt-2");
+        searchBtn.classList.add("bx-x");
+    }
+    else {
+        searchBtn.classList.remove("bx-x");
+        searchBtn.classList.add("bx-search-alt-2");
+    }
+}
+cartBtn.onclick = function () {
+    // Toggles Active Class In Search Form On Click
+    document.querySelector("#cart").classList.toggle("active");
+    // Changes Icons on Click
+    if (document.querySelector("#cart").classList.contains('active')) {
+        cartBtn.classList.remove("bx-cart");
+        cartBtn.classList.add("bx-x");
+    }
+    else {
+        cartBtn.classList.remove("bx-x");
+        cartBtn.classList.add("bx-cart");
+    }
+}
+
+
+darkBtn.onclick = function () {
+    // Toggles Active Class In Cart Form On Click
+    document.body.classList.toggle("dark-mode");
+    // If Body Contains Dark Mode Class
+    if (document.body.classList.contains('dark-mode')) {
+
+        // Changes Darks Mode Button Icon
+        darkBtn.classList.remove("bx-moon");
+        darkBtn.classList.add("bx-sun");
+        // Changes Sign Img For Dark Background
+        signImg.src = 'Images/sign/sign-dark.png';
+    }
+    else {
+        darkBtn.classList.remove("bx-sun");
+        darkBtn.classList.add("bx-moon");
+
+        // Changes Sign Img For Light Background
+        signImg.src = 'Images/sign/sign-light.png';
+
+    }
+}
+
+// Menu Section 
+let menuTabs = document.querySelector('.menu-tabs');
+menuTabs.addEventListener("click", function (e) {
+    
+    // If Clicked Tab Does Not Contains Active Class
+    if (e.target.classList.contains("menu-tab-item") && !e.target.classList.contains('active')) {
+        
+        // Gets Data Attribute 
+        const target = e.target.getAttribute("data-target");
+        
+        // Removes Active Class From Active Tab
+        menuTabs.querySelector(".active").classList.remove("active");
+        
+        // Add active class from clicked Tab
+        e.target.classList.add("active");
+        
+        let menuSection = document.querySelector(".menu-section");
+        
+        // Removes Show Class Form Active Tab Content
+        menuSection.querySelector(".menu-tab-content.show").classList.remove("show");
+        
+        // Adds Show Class To Clicked Tab Content
+        menuSection.querySelector(target).classList.add("show");
+        
+    }
+    // If Clicked Tab Contains Active Class
+    else {
+        return // Returns Nothing
+    }
+});
+
+// Events Carousel
+$('.events-carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    dots: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        750: {
+            items: 2
+        },
+        1000: {
+            items: 2
+        }
+    }
+})
+// Team Carousel
+$('.team-carousel').owlCarousel({
+    loop: true,
+    margin: 20,
+    dots: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
+// Review Carousel
+
+$('.reviews-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    dots: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
+
+// Blog Carousel
+$('.blog-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    dots: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        750: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+}) 
